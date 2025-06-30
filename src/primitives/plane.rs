ceb2983acc0dee52feb68f2673b7c2061e271138
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2025-07-01 00:19:16 +0900

    fmt: ふぉ＾〜

--- src/primitives/plane.rs
+++ src/primitives/plane.rs
@@ -1,9 +1,9 @@
-use glam::Vec3;
-use crate::{Hittable, HitRecord, Ray, Material}; // main.rsから移動させる共通定義をインポート
+use crate::{HitRecord, Hittable, Material, Ray};
+use glam::Vec3; // main.rsから移動させる共通定義をインポート
 
 #[derive(Debug, Clone, Copy)]
 pub struct Plane {
-    pub point: Vec3, // 平面上の任意の点
+    pub point: Vec3,  // 平面上の任意の点
     pub normal: Vec3, // 平面の法線
     pub material: Material,
 }
@@ -30,14 +30,24 @@ impl Hittable for Plane {
 
         // 衝突点の座標を計算
         let point = ray.origin + t * ray.direction;
-        
+
         // レイが表面から当たったか、裏面から当たったかを判定
         let front_face = ray.direction.dot(self.normal) < 0.0;
         // 法線ベクトルは常にレイと向かい合うように調整
-        let normal = if front_face { self.normal } else { -self.normal };
-        
-        let hit_record = HitRecord { t, point, normal, front_face ,material: self.material};
-        
+        let normal = if front_face {
+            self.normal
+        } else {
+            -self.normal
+        };
+
+        let hit_record = HitRecord {
+            t,
+            point,
+            normal,
+            front_face,
+            material: self.material,
+        };
+
         // ★★★ 変更点 ★★★
         // 単一のHitRecordを、要素が1つのVec（ベクタ）に入れてSomeで返す
         Some(vec![hit_record])
